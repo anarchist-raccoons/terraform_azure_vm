b655b44a0233f13d376b5354ac72e708bee731e9
Julie Allinson <julie.allinson@london.ac.uk>
2019-06-17 20:22:02 +0000

    return to using gh module

--- templates/main.tf
+++ templates/main.tf
@@ -1,7 +1,6 @@
 # with AZURE
 module "azure_vm" {
-  #source =  "git::https://github.com/anarchist-raccoons/terraform_azure_vm.git?ref=master"
-  source = "../"
+  source =  "git::https://github.com/anarchist-raccoons/terraform_azure_vm.git?ref=master"
   
   subscription_id = "${var.subscription_id}"
   tenant_id = "${var.tenant_id}"
